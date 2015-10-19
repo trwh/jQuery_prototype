2d15e00435ab1b5df45e9d13a6b5e9d595d6d831
Tim Harrison <mail@trwh.co.uk>
2015-10-19 13:52:57 +0100

    Version 0

---
+++ js/gitprofile.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  $('.gitprofile').on('submit', function(e) {
+
+    e.preventDefault();
+
+     // Replace this with a Github access token that you can generate as explained here https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+    var url = 'https://api.github.com/users/' + $('input.username').val() + "?access_token=" + access_token;
+
+    var template = $('template').html();
+
+    $.get(url, function(info) {
+      console.log(info);
+      $('.container').prepend(Mustache.render(template, info));
+    }).fail(function() {
+      $('.container').prepend("User not found")
+    }).always(function() {
+      $('input.username').val('');
+      });
+  });
+});
